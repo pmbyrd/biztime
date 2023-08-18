64db8ca6c7f71f6508dfecf321566e3d58fc7396
Perriesha <pmbyrd@gmail.com>
2023-08-17 19:17:46 -0700

    invoice testing needs work

--- routes/invoices.test.js
+++ routes/invoices.test.js
@@ -1,22 +1,93 @@
 
-// process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
-// const request = require('supertest');
-// const app = require('../app');
-// const db = require('../db');
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db');
 
-// let testInvoice;
+beforeEach(async () => {
+    testResults = await db.query(`
+        SELECT id, comp_code, amt, paid, add_date, paid_date
+        FROM invoices
+    `);
+});
 
-// beforeEach(async () => {
-//     const result = await db.query(
-//         `INSERT INTO invoices (comp_code, amt) VALUES ('apple', 100) RETURNING id, comp_code, amt, paid, add_date, paid_date`);
-//     testInvoice = result.rows[0];
-//     });
+afterAll(async () => {
+    await db.end();
+    });
+    
+describe('GET /invoices', () => {
+    test('Get a list with all invoices', async () => {
+        const res = await request(app).get('/invoices');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ invoices: testResults.rows });
+    });
+});
 
-// afterEach(async () => {
-//     await db.query(`DELETE FROM invoices`);
-//     });
+describe('GET /:id', () => {
+    test('Get a single invoice', async () => {
+        const response = await request(app).get("/invoices/1");
+    expect(response.body).toEqual(
+        {
+          "invoice": {
+            id: 1,
+            amt: 100,
+            add_date: '2018-01-01T08:00:00.000Z',
+            paid: false,
+            paid_date: null,
+            company: {
+              code: 'apple',
+              name: 'Apple',
+              description: 'Maker of OSX.',
+            }
+            }
+        }
+    );
+    });
+});
 
-// afterAll(async () => {
-//     await db.end();
-//     });
+describe('POST /', () => {
+    test('Create a new invoice', async () => {
+        const res = await request(app)
+            .post('/invoices')
+            .send({ comp_code: 'apple', amt: 1000 });
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual({
+            invoice: {
+                id: expect.any(Number),
+                comp_code: 'apple',
+                amt: 1000,
+                paid: false,
+                add_date: expect.any(String),
+                paid_date: null
+            }
+        });
+    });
+});
+
+describe('PUT /:id', () => {
+    test('Update an invoice', async () => {
+        const res = await request(app)
+            .put('/invoices/1')
+            .send({ amt: 500 });
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({
+            invoice: {
+                id: 1,
+                comp_code: 'apple',
+                amt: 500,
+                paid: false,
+                add_date: expect.any(String),
+                paid_date: null
+            }
+        });
+    });
+});
+
+describe('DELETE /:id', () => {
+    test('Delete an invoice', async () => {
+        const res = await request(app).delete('/invoices/1');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ status: 'deleted' });
+    });
+});
